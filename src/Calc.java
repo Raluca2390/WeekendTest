c3a7a07abb37fc8a556ee1062e887da67271cd80
Serbanescu <ralucang@gmail.com>
2017-02-25 10:54:54 +0200

    first java project to add 2 nr

---
+++ src/Calc.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Serbanescu on 25.02.2017.
+ */
+public class Calc {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/Example1.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Serbanescu on 25.02.2017.
+ */
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("HEllo!");
+
+        int sum=Calc.add(2,3);
+        System.out.println(sum);
+
+    }
+    /*metoda noua */
+
+
+
+}
+/* metodele "add" se apeleaza numai din clasa respectiva, clasa calc nu e publica*/
+/*se poate apela si din clasa publica Calc noua*/
+
